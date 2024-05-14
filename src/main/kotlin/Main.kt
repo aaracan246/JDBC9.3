0d7497c7f5959909972e21119c59fa48bdc8c3c9
morri <aaracan246@g.educaand.es>
2024-05-14 18:41:32 +0200

    Donev2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 package org.example
 
+import org.example.inputOutput.Console
 import org.example.products.Product
 import org.example.products.ProductDAO
 
 fun main() {
 
+    val console = Console()
+
     val product = Product(1, "Smartphone", 999.99f, "The latest smartphone model", "Apple", "Electronics")
 
-    val productId = ProductDAO().createProduct(product)
+    val productId = ProductDAO(console).createProduct(product)
 
-    println("Product ID: $productId")
+    console.writer("Product ID: $productId")
 
 }
